cd4edc8c2e75b9647bd34116916124f9c23f4e2c
Yusrâ Arigui <y.arigui@interface3.be>
2023-11-09 16:22:17 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/project/public/build')
+    .setPublicPath('/project1/public/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
